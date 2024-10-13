6df54ad6d4692274c8ad7a4a68e86a372921b65d
RobRob08 <tdroki60@gmail.com>
2024-10-13 15:16:45 +0800

    Add files via upload

---
+++ PAKUMAN/src/Pacman.java
@@ -0,0 +1,18 @@
+import javax.swing.JFrame; 
+
+public class Pacman extends JFrame{
+
+
+	public Pacman() {
+		add(new Model()); 
+	}
+
+	public static void main(String[] args) {
+		Pacman pac = new Pacman(); 
+		pac.setVisible(true);
+		pac.setTitle("Pacman");
+		pac.setSize(700, 900);
+		pac.setDefaultCloseOperation(EXIT_ON_CLOSE);
+		pac.setLocationRelativeTo(null);
+	}
+}
